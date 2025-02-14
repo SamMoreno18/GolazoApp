c2037eac806ab38955090aed86c0397785f6c2f8
Leah <leahvcm.01@gmail.com>
2025-02-14 09:29:53 -0600

    Creación de módulos NestJS
    Se crearon 3 módulos para los distintos servicios que ofrece la applicación:
    -Matches: información de partidos.
    -Users: validación de usuarios.
    -Locations: Servicios de terceros como hospedaje, transporte y restaurantes

--- golazoapi/src/app.module.ts
+++ golazoapi/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MatchesModule } from './matches/matches.module';
+import { LocationsModule } from './locations/locations.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, MatchesModule, LocationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
