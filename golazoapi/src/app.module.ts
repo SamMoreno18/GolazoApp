c55d2d4940ffe2bc89bb04ae2b5312824c94b826
Leah <leahvcm.01@gmail.com>
2025-03-23 00:39:47 -0600

    Importaciones NodeJS
    -Guardado dentro de package.json los módulos que se deben instalar para el funcionamiento
    
    -Eliminado la importación de módulos inexistentes (weather, transport, food y results)

--- golazoapi/src/app.module.ts
+++ golazoapi/src/app.module.ts
@@ -15,15 +15,8 @@ import { LodgingModule } from './lodging/lodging.module';
 
 import { MatchesController } from './matches/matches.controller';
 import { MatchesService } from './matches/matches.service';
-import { WeatherController } from './weather/weather.controller';
-import { WeatherService } from './weather/weather.service';
 import { LodgingController } from './lodging/lodging.controller';
 import { LodgingService } from './lodging/lodging.service';
-import { TransportController } from './transport/transport.controller';
-import { TransportService } from './transport/transport.service';
-import { FoodController } from './food/food.controller';
-import { FoodService } from './food/food.service';
-import { ResultsController } from './results/results.controller';
 import { ResultsService } from './results/results.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
@@ -44,20 +37,13 @@ import { UsersService } from './users/users.service';
   controllers: [
     AppController,
     MatchesController,
-    WeatherController,
     LodgingController,
-    TransportController,
-    FoodController,
-    ResultsController,
     UsersController,
   ],
   providers: [
     AppService,
     MatchesService,
-    WeatherService,
     LodgingService,
-    TransportService,
-    FoodService,
     ResultsService,
     UsersService,
   ],
