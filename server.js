b4a9eb0d7c02bcb7a5b7f4ea7937f9489ced0083
IvanECL <137136161+IvanECL@users.noreply.github.com>
2025-03-23 12:47:59 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <title>Document</title>
-    <h1>My first PHP page</h1>
-</body>
-</html>
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("¡Servidor Node.js funcionando!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
