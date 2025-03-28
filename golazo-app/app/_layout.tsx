2a0c828aa9acef8e880dec231ace0198c609b191
Kevin Torres <kevintu236@gmail.com>
2025-03-28 08:36:57 -0600

    Removida la primera versión de las interfaces
    - Removí la primera versión de las interfaces para mantener la segunda, que contiene React Native Reusables

--- golazo-app/app/_layout.tsx
+++ golazo-app/app/_layout.tsx
@@ -1,39 +1,82 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
+import '../global.css'
+
+import { DarkTheme, DefaultTheme, Theme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import * as React from 'react';
+import { Platform } from 'react-native';
+import { NAV_THEME } from '~/lib/constants';
+import { useColorScheme } from '~/lib/useColorScheme';
+import { PortalHost } from '@rn-primitives/portal';
+import { ThemeToggle } from '~/components/ThemeToggle';
+import { setAndroidNavigationBar } from '~/lib/android-navigation-bar';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Screen from './(drawer)';
+import Turismo from './(drawer)/turismo';
+import Hoteles from './(drawer)/hoteles';
+import Perfil from './(drawer)/perfil';
+import Transporte from './(drawer)/transporte';
+import Login from './(drawer)/login';
+import CrearCuenta from './(drawer)/crear-cuenta';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+const Drawer = createDrawerNavigator();
+
+export {
+  ErrorBoundary,
+} from 'expo-router';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  const hasMounted = React.useRef(false);
+  const { colorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+  useIsomorphicLayoutEffect(() => {
+    if (hasMounted.current) {
+      return;
     }
-  }, [loaded]);
 
-  if (!loaded) {
+    if (Platform.OS === 'web') {
+      document.documentElement.classList.add('bg-background');
+    }
+    setAndroidNavigationBar(colorScheme);
+    setIsColorSchemeLoaded(true);
+    hasMounted.current = true;
+  }, []);
+
+  if (!isColorSchemeLoaded) {
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Drawer.Navigator
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: '#f4f4f4',
+          width: 240,
+        },
+        drawerActiveTintColor: '#ED4352',
+        drawerInactiveTintColor: '#000',
+      }}
+    >
+      <Drawer.Screen name="Inicio" component={Screen} />
+      <Drawer.Screen name="Lugares Interesantes" component={Turismo} />
+      <Drawer.Screen name="Buscar Hoteles" component={Hoteles} />
+      <Drawer.Screen name="Rutas de Transporte" component={Transporte} />
+      <Drawer.Screen name="Mi perfil" component={Perfil} />
+      <Drawer.Screen name="Login" component={Login} />
+      <Drawer.Screen name="Crear cuenta" component={CrearCuenta} />
+    </Drawer.Navigator>
   );
 }
+
+const useIsomorphicLayoutEffect =
+  Platform.OS === 'web' && typeof window === 'undefined' ? React.useEffect : React.useLayoutEffect;
