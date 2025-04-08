6471f95bc9e2fad4c21797fb964b55acb34f5a97
Kevin Torres <kevintu236@gmail.com>
2025-04-08 15:17:46 -0600

    Estilos arreglados
    Se importó el archivo "global.css" en cada una de las páginas del layout.

--- golazoapp/app/(app)/_layout.tsx
+++ golazoapp/app/(app)/_layout.tsx
@@ -1,3 +1,5 @@
+import '../../global.css'
+
 import { Drawer } from "expo-router/drawer";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
@@ -9,20 +11,14 @@ import { View } from 'react-native';
 export default function AppLayout() {
   const { session, isLoading } = useSession();
 
-  // You can keep the splash screen open, or render a loading screen like we do here.
   if (isLoading) {
     return <Text>Loading...</Text>;
   }
 
-  // Only require authentication within the (app) group's layout as users
-  // need to be able to access the (auth) group and sign in again.
   if (!session) {
-    // On web, static rendering will stop here as the user is not authenticated
-    // in the headless Node process that the pages are rendered in.
     return <Redirect href="/login" />;
   }
 
-  // This layout can be deferred because it's not the root layout.
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Drawer screenOptions={{
